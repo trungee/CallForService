840fbb095f17e61e990292e355f1b8b738778f09
trung.nguyen <trung_nguyen@epam.com>
2021-03-27 13:30:05 +0700

    add ERD

---
+++ csf_service/src/test/java/com/github/atha/csf/CallForServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.atha.csf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallForServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
